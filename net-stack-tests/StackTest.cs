f2ede89e739b5aab8bdc0e71047633b7263d0f10
unknown <mgisb003@hotmail.com>
2020-09-26 23:41:37 -0400

    answers for interview

--- net-stack-tests/StackTest.cs
+++ net-stack-tests/StackTest.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using net_stack;
+<<<<<<< Updated upstream
 using net_stack.exceptions;
+=======
+using net_stack.model.exceptions;
+>>>>>>> Stashed changes
 
 namespace net_stack_tests
 {
@@ -16,6 +20,7 @@ namespace net_stack_tests
         }
 
         [TestMethod]
+<<<<<<< Updated upstream
         public void isEmpty_OnNewStack_ShouldReturnTrue()
         {
             Assert.AreEqual(true, stack.isEmpty());
@@ -25,11 +30,27 @@ namespace net_stack_tests
         public void isEmpty_OnePush_ShouldReturnFalse()
         {
             stack.push("abc");
+=======
+        public void isEmpty_ShouldReturnTrue_IfStackIsEmpty()
+        {
+            Assert.AreEqual(true, stack.isEmpty());
+
+        }
+
+        [TestMethod]
+        public void isEmpty_ShouldReturnFalse_IfNotEmpty()
+        {
+            stack.push(1);
+>>>>>>> Stashed changes
             Assert.AreEqual(false, stack.isEmpty());
         }
 
         [TestMethod]
+<<<<<<< Updated upstream
         public void isEmpty_ShouldReturnTrueAfterPushAndPop()
+=======
+        public void isEmpty_ShouldReturnTrueAfterPushAndPopIsCalled()
+>>>>>>> Stashed changes
         {
             stack.push(1);
             stack.pop();
@@ -37,6 +58,7 @@ namespace net_stack_tests
         }
 
         [TestMethod]
+<<<<<<< Updated upstream
         public void peek_ShouldReturnLastValuePushed()
         {
             stack.push("abc");
@@ -51,6 +73,22 @@ namespace net_stack_tests
 
         [TestMethod]
         public void peek_ShouldReturnNullAfterPushAndPop()
+=======
+        public void peek_ShouldReturnNull_IfStackIsEmpty()
+        {
+            Assert.AreEqual(null, stack.peek());
+        }
+
+        [TestMethod]
+        public void peek_ShouldReturnMostRecentValuePushed()
+        {
+            stack.push(1);
+            Assert.AreEqual(1, stack.peek());
+        }
+
+        [TestMethod]
+        public void peek_ShouldReturnNull_AfterPushAndPop()
+>>>>>>> Stashed changes
         {
             stack.push(1);
             stack.pop();
@@ -59,19 +97,28 @@ namespace net_stack_tests
 
         [TestMethod]
         [ExpectedException(typeof(EmptyStackException))]
+<<<<<<< Updated upstream
         public void pop_ThrowsException_newStack()
+=======
+        public void pop_ThrowsEmptyStackException_IfStackIsEmpty()
+>>>>>>> Stashed changes
         {
             stack.pop();
         }
 
         [TestMethod]
+<<<<<<< Updated upstream
         public void pop_ShouldReturnLastValuePushed()
+=======
+        public void pop_ReturnsMostRecentValuePushed()
+>>>>>>> Stashed changes
         {
             stack.push(1);
             Assert.AreEqual(1, stack.pop());
         }
 
         [TestMethod]
+<<<<<<< Updated upstream
         public void pop_ShouldReturnPushedValuesReversed()
         {
             stack.push("a");
@@ -84,10 +131,23 @@ namespace net_stack_tests
 
         [TestMethod]
         public void contains_PushMultipleValuesAndReturnTrueForThoseValues()
+=======
+        public void pop_ReturnsLastValuePushedWhenMultipleValuesPushed()
+        {
+            stack.push(1);
+            stack.push(2);
+            Assert.AreEqual(2, stack.pop());
+            Assert.AreEqual(1, stack.peek());
+        }
+
+        [TestMethod]
+        public void contains_ReturnTrueIfPushedValueIsPresent()
+>>>>>>> Stashed changes
         {
             stack.push("1");
             stack.push("2");
             stack.push("3");
+<<<<<<< Updated upstream
             Assert.AreEqual(true, stack.contains("1"));
             Assert.AreEqual(true, stack.contains("2"));
             Assert.AreEqual(true, stack.contains("3"));
@@ -103,6 +163,22 @@ namespace net_stack_tests
             Assert.AreEqual(false, stack.contains(5));
             Assert.AreEqual(false, stack.contains(6));
         }
+=======
+            Assert.AreEqual(true, stack.contains("3"));
+            Assert.AreEqual(true, stack.contains("2"));
+            Assert.AreEqual(true, stack.contains("1"));
+        }
+
+        [TestMethod]
+        public void contains_ReturnFalseIfValueNotIsPresent()
+        {
+            stack.push("1");
+            Assert.AreEqual(false, stack.contains("4"));
+
+        }
+
+
+>>>>>>> Stashed changes
     }
 }
 
--- net-stack/Stack.cs
+++ net-stack/Stack.cs
@@ -1,12 +1,37 @@
+<<<<<<< Updated upstream
 ﻿using net_stack.exceptions;
+=======
+﻿using net_stack.model.exceptions;
+>>>>>>> Stashed changes
 using System;
 namespace net_stack
 {
 
+<<<<<<< Updated upstream
     public class Stack
   {
         private Node head = null;
 
+=======
+    public class Node
+    {
+        public Node(object value, Node next)
+        {
+            Value = value;
+            Next = next;
+        }
+
+        public object Value { get; private set; }
+        public Node Next { get; set; }
+
+    }
+    public class Stack
+    {
+
+        private Node head;
+        object obj;
+        private int size = 0;
+>>>>>>> Stashed changes
         public bool isEmpty()
         {
             return head == null;
@@ -14,21 +39,37 @@ namespace net_stack
 
         public void push(object o)
         {
+<<<<<<< Updated upstream
+=======
+            size++;
+>>>>>>> Stashed changes
             Node node = new Node(o, null);
             node.Next = head;
             head = node;
         }
 
+<<<<<<< Updated upstream
         public string peek()
+=======
+        public object peek()
+>>>>>>> Stashed changes
         {
             if (isEmpty())
             {
                 return null;
+<<<<<<< Updated upstream
             }
             return "abc";
         }
 
         public Object pop()
+=======
+            }       
+            return head.Value;
+        }
+
+        public object pop()
+>>>>>>> Stashed changes
         {
             if (isEmpty())
             {
@@ -41,18 +82,32 @@ namespace net_stack
 
         public bool contains(object o)
         {
+<<<<<<< Updated upstream
             Node iterator = head;
             while (iterator != null)
             {
                 if (iterator.Value == o)
+=======
+
+            Node iterator = head;
+            while(iterator != null)
+            {
+                if(iterator.Value == o)
+>>>>>>> Stashed changes
                 {
                     return true;
                 }
                 iterator = iterator.Next;
             }
+<<<<<<< Updated upstream
 
             return false;
 
         }
+=======
+            return false;
+        }
+
+>>>>>>> Stashed changes
     }
 }
